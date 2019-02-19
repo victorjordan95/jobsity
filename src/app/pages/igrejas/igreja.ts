48c755b6fe10e4e3dfd8e21a2a27aa96b687efe6
Victor Jordan <victorjordan95@gmail.com>
2019-02-19 15:42:58 -0300

    fix bug

--- src/app/pages/igrejas/igreja.ts
+++ src/app/pages/igrejas/igreja.ts
@@ -2,14 +2,14 @@ import { Tecnico } from '../tecnico/tecnico';
 
 export class Igreja {
     id: any;
-    codCCB: any;
-    nomeIgreja: String;
-    rua: String;
-    numero: number;
-    bairro: String;
-    cep: any;
-    cidade: String;
-    uf: any;
+    codCCB: any = '';
+    nomeIgreja: String = '';
+    rua: String  = '';
+    numero: number = null;
+    bairro: String = '';
+    cep: any = '';
+    cidade: String = '';
+    uf: any  = '';
     tecnico: String = '';
     numCel:  number = null;
     marca: String = '';
